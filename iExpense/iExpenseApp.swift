156b1453c62cbd42b9a6866ef7c20d5af319fbac
David Bailey <davidbailey00@outlook.com>
2021-06-02 09:14:33 +0100

    initial commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by David Bailey on 02/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
