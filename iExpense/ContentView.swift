c08e19a4a4fccee6ce22119407ba6139c272bdd4
David Bailey <davidbailey00@outlook.com>
2021-06-03 15:13:43 +0100

    Create list of expenses with add button

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -7,22 +7,47 @@
 
 import SwiftUI
 
-struct User: Codable {
-    var firstName: String
-    var lastName: String
+struct ExpenseItem {
+    let name: String
+    let type: String
+    let amount: Int
+}
+
+class Expenses: ObservableObject {
+    @Published var items = [ExpenseItem]()
 }
 
 struct ContentView: View {
-    @State private var user = User(firstName: "Taylor", lastName: "Swift")
+    @ObservedObject var expenses = Expenses()
 
     var body: some View {
-        Button("Save User") {
-            let encoder = JSONEncoder()
-
-            if let data = try? encoder.encode(self.user) {
-                UserDefaults.standard.set(data, forKey: "UserData")
+        NavigationView {
+            List {
+                ForEach(expenses.items, id: \.name) { item in
+                    Text(item.name)
+                }
+                .onDelete(perform: removeItems)
+            }
+            .navigationTitle("iExpense")
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button(action: {
+                        let expense = ExpenseItem(
+                            name: "Test",
+                            type: "Personal",
+                            amount: 5
+                        )
+                        expenses.items.append(expense)
+                    }) {
+                        Image(systemName: "plus")
+                    }
+                }
             }
         }
+    }
+
+    func removeItems(at offsets: IndexSet) {
+        expenses.items.remove(atOffsets: offsets)
     }
 }
 
